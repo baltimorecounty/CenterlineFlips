9fd56477bcd16156c5214c83e79c5f99453117ad
Elliott Plack <elliott.plack@gmail.com>
2013-12-06 13:20:45 -0500

    added comments to the script

--- script/swapCL-AddressRanges.py
+++ script/swapCL-AddressRanges.py
@@ -1,18 +1,31 @@
+# Script to flip attributes on a centerline.
+# arcpy.da.SearchCursor requires ArcGIS 10.1 or higher
+# coding help via GIS StackExchange:
+# http://gis.stackexchange.com/a/79625/15499
+# http://gis.stackexchange.com/questions/79615/is-there-a-programmatic-way-to-swap-attributes-on-a-feature/79625?noredirect=1#comment109573_79625
+
 import arcpy
 
+# set workspace to SDE connection
 workspace = r"Database Connections\5160Facilities.sde"
 
+# set the fields to the ones being flipped
 fields = (["FROMLEFTP", "TORIGHTP", "TOLEFTP", "FROMRIGHTP",
            "FROMLEFTA", "TORIGHTA", "TOLEFTA", "FROMRIGHTA",])
 
+# set the feature class to the SDE feature class name
+# use ListFeatureClasses ({wild_card}, {feature_type}, {feature_dataset})
+# 	to get a list of feature classes
 fc = "FACILITIES.Centerline"
 
+# searches for the cursors to verify the correct data is selected
 with arcpy.da.SearchCursor(fc, fields) as cursor:
     for row in cursor:
         print(("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}"
                .format(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])))
 
+# loops through selected rows and swaps the values
 with arcpy.da.UpdateCursor(fc, fields) as cursor:
     for row in cursor:
 
