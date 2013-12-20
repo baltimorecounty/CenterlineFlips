6548ce2605cae75b2355cc67e265a3fb64e5a351
Elliott Plack <elliott.plack@gmail.com>
2013-12-20 14:42:25 -0500

    testing an implementation of #2 and #3

--- script/FieldCalcRowLogic_test.py
+++ script/FieldCalcRowLogic_test.py
@@ -2,6 +2,8 @@ fc_shp = "CENTERLINE_FLIPS"
 field = "FLIP"
 desc = arcpy.Describe(fc_shp)
 if not desc.FIDSet  == '':
-	print 'Flip would happen'
+	arcpy.CalculateField_management("CENTERLINE_FLIPS","FLIP",'1')
 else:
-	arcpy.AddMessage("Error.No {0} features are selected.".format(fc_shp))
+	arcpy.AddError("Error.No {0} features are selected.".format(fc_shp))
+	
+print(arcpy.GetMessages())
