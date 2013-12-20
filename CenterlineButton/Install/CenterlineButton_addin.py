bef320bb7007b2c0e2d285514a499f8068d1be23
Elliott Plack <elliott.plack@gmail.com>
2013-12-20 14:53:42 -0500

    putting things in place for v0.4
    fixes #2 and #3

--- CenterlineButton/Install/CenterlineButton_addin.py
+++ CenterlineButton/Install/CenterlineButton_addin.py
@@ -7,30 +7,46 @@ class SwapCenterlines(object):
         self.enabled = True
         self.checked = False
     def onClick(self):
-        # set workspace to SDE connection
-		workspace = r"Database Connections\facilities@5160_93.sde"
-
-		# set the fields to the ones being flipped
-		fields = (["FROMLEFTP", "TORIGHTP", "TOLEFTP", "FROMRIGHTP",
-				   "FROMLEFTA", "TORIGHTA", "TOLEFTA", "FROMRIGHTA",])
-
-		# set the feature class to the SDE feature class name
-		# use ListFeatureClasses ({wild_card}, {feature_type}, {feature_dataset})
-		# 	to get a list of feature classes
-		fc = "FACILITIES.Centerline"
+        # Script to flip attributes on a centerline.
+		# arcpy.da.SearchCursor requires ArcGIS 10.1 or higher
+		# coding help via GIS StackExchange:
+		# http://gis.stackexchange.com/a/79625/15499
+		# http://gis.stackexchange.com/questions/79615/is-there-a-programmatic-way-to-swap-attributes-on-a-feature/79625?noredirect=1#comment109573_79625
 		
-		# flips the line direction
-		arcpy.FlipLine_edit("FACILITIES.Centerline")
+		try:
+			workspace = r"Database Connections\facilities@5160_93.sde" # set workspace to SDE connection
+			fields = (["FROMLEFTP", "TORIGHTP", "TOLEFTP", "FROMRIGHTP", "FROMLEFTA",
+					   "TORIGHTA", "TOLEFTA", "FROMRIGHTA", "TRAFFIC_FLOW"]) # set the fields to the ones being flipped
+			fc = "FACILITIES.Centerline" # set the feature class to the SDE feature class name
+			fc_shp = "CENTERLINE_FLIPS" # feature class to calculate
+			field = "FLIP" # calc filed to change
+			desc = arcpy.Describe(fc_shp) # describe selection of fc_shape
 
-		# loops through selected rows and swaps the values
-		with arcpy.da.UpdateCursor(fc, fields) as cursor:
-			for row in cursor:
+			if not desc.FIDSet  == '': # an fc_shp is selected
+				# Marked the discrepancy as done in the CENTERLINE_FLIPS tracking shapefile
+				arcpy.CalculateField_management(fc_shp,field,'1')
+				
+				# loops through selected rows and swaps the values
+				with arcpy.da.UpdateCursor(fc, fields) as cursor:
+					for row in cursor:
+						# update swaps
+						row[0], row[1] = row[1], row[0]
+						row[2], row[3] = row[3], row[2]
+						row[4], row[5] = row[5], row[4]
+						row[6], row[7] = row[7], row[6]
+						
+						# swap directions
+						if row[8] == 'F':
+							row[8] = 'R'
+							cursor.updateRow(row)
+						elif row[8] == 'R':
+							row[8] = 'F'
+							cursor.updateRow(row)
+						else:
+							cursor.updateRow(row)
 
-				row[0], row[1] = row[1], row[0]
-				row[2], row[3] = row[3], row[2]
-				row[4], row[5] = row[5], row[4]
-				row[6], row[7] = row[7], row[6]
-
-				cursor.updateRow(row)
-		# Mark the discrepancy as done in the CENTERLINE_FLIPS tracking shapefile
-		arcpy.CalculateField_management("CENTERLINE_FLIPS","FLIP",'1')
+			else:
+				arcpy.AddError("Error. No {0} features are selected.".format(fc_shp))
+				print(arcpy.GetMessages())
+		except Exception as e:
+			arcpy.AddError(e.message)
